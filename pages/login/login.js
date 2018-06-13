6153f0fb4f6edad86822f1588f2f88d0702b02d2
weiweikate <690125575@qq.com>
2018-06-13 20:14:11 +0800

    搜索

--- pages/login/login.js
+++ pages/login/login.js
@@ -11,8 +11,8 @@ Page({
   onLoad: function (options) {
   
   },
-  onReady: function () {
-  
+  onShow: function () {
+
   },
   isSeePwd(){
     this.setData({
@@ -24,50 +24,13 @@ Page({
     let page = n == 1 ? '/pages/register/register' :'/pages/find-password/step1/step1'
     Tool.navigateTo(page)
   },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    let params = e.detail.value
+    let r = RequestFactory.findMemberByPhone(params);
+    r.finishBlock = (req) => {
+      console.log(req)
+    }
+    // r.addToQueue();
   },
-  bindtap(e){
-    
-  },
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
