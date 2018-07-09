5ce1924e4ab666705a725dc15207aa150694f47c
weiweikate <690125575@qq.com>
2018-07-09 16:38:49 +0800

    新增首页页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,6 +11,23 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
+    url:'https://dnlcrm.oss-cn-beijing.aliyuncs.com/xcx/',
+    classify:[
+      { name: '美容美妆', imgurl:'index-icon-1.png'},
+      { name: '包包', imgurl: 'index-icon-2.png' },
+      { name: '鞋子', imgurl: 'index-icon-3.png' },
+      { name: '服饰', imgurl: 'index-icon-4.png' },
+    ],
+    recommendImgUrl:[
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    topicImgUrl: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ]
   },
   onLoad: function () {
 
