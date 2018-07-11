3c6c323df2d2dfb4d5dd4ca01063e98aa3d74bf8
wangchang <185483658@qq.com>
2018-07-11 09:53:33 +0800

    问题反馈接口调试 待提现bug修复

--- pages/my/help-customer/questionFeedback/questionFeedback.js
+++ pages/my/help-customer/questionFeedback/questionFeedback.js
@@ -6,53 +6,104 @@ Page({
      * 页面的初始数据
      */
     data: {
-      count:0,
-      active:false,
-      chooseType:false,
-      mask:false,
-      index:0,
-      type:'请选择问题类型',
-      success:false
+        count: 0,
+        active: false,
+        chooseType: false,
+        mask: false,
+        index: 0,
+        type: '请选择问题类型',
+        success: false,
+        originalImg: [],
+        smallImg: [],
+        content:''
     },
     //选择问题类型弹窗
-    questionType(){
+    questionType() {
         this.setData({
-            mask:true
+            mask: true
         })
     },
     //选择问题类型
-    chooseType(e){
-       let index=e.currentTarget.dataset.index;
-       let content=e.currentTarget.dataset.content;
-       this.setData({
-           index:index,
-           mask:false,
-           type:content
-       });
+    chooseType(e) {
+        let index = e.currentTarget.dataset.index;
+        let content = e.currentTarget.dataset.content;
+        this.setData({
+            index: index,
+            mask: false,
+            type: content
+        });
         this.active();
     },
     //计数
     detailRemark(e) {
-        let length=e.detail.value.length;
+        let length = e.detail.value.length;
         this.setData({
-            count:length
+            count: length,
+            content:e.detail.value
         });
         this.active();
     },
-    //提交成功
-    sure(){
+    //添加图片
+    uploadImg() {
+        let callBack = (fileInfo) => {
+            let tempUrl = fileInfo.data.imageUrl;
+            let tempThumbUrl = fileInfo.data.imageThumbUrl;
+            this.data.originalImg.push(tempUrl);
+            this.data.smallImg.push(tempThumbUrl);
+            this.setData({
+                originalImg:this.data.originalImg,
+                smallImg:this.data.smallImg
+            })
+        };
+        Tool.uploadImage(1, callBack)
+    },
+    //删除图片
+    deleteImg(e){
+        let index=e.currentTarget.dataset.index;
+        this.data.originalImg.splice(index,1);
+        this.data.smallImg.splice(index,1);
         this.setData({
-            success:false
+            originalImg:this.data.originalImg,
+            smallImg:this.data.smallImg
         })
     },
-    active(){
-        if(this.data.index!=0&&this.data.count!=0){
+    //提交成功
+    addFeedback() {
+        if (this.data.active) {
+            let params = {
+                content: this.data.content,
+                type: this.data.index,
+            };
+            if(this.data.originalImg){
+                params.originalImg=this.data.originalImg.join(',');
+                params.smallImg=this.data.smallImg.join(',')
+            }
+            let r = RequestFactory.addFeedback(params);
+            r.finishBlock = (req) => {
+                if(req.responseObject.code==200){
+                    this.setData({
+                        success:true
+                    })
+                }
+            };
+            r.addToQueue();
+        }
+    },
+    //确定
+    sure(){
+      this.setData({
+          success:false
+      });
+      Tool.navigateTo('../help-customer')
+    },
+    active() {
+        if (this.data.index != 0 && this.data.count != 0) {
             this.setData({
-                active:true
+                active: true
             })
-        }else{
+        } else {
             this.setData({
-                active:false
+                active: false
             })
         }
     },
