279f2a414fa3c8f0b72dfa312fffa78ce5107967
weiweikate <690125575@qq.com>
2018-07-30 20:34:11 +0800

    新增专题模块

---
+++ pages/topic/topic.js
@@ -0,0 +1,10 @@
+let { Tool, RequestFactory } = global;
+Page({
+  data: {
+    topicImgUrl:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+    typeId:2,
+  },
+  onLoad: function (options) {
+  
+  },
+})
