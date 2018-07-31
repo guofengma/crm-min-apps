1762dd35d8f0d2ef7b060150227b609fa6a60e90
weiweikate <690125575@qq.com>
2018-07-31 09:34:30 +0800

    调试专题页面模板

--- pages/topic/topic.js
+++ pages/topic/topic.js
@@ -2,7 +2,7 @@ let { Tool, RequestFactory } = global;
 Page({
   data: {
     topicImgUrl:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-    typeId:2,
+    typeId:4,
   },
   onLoad: function (options) {
   
